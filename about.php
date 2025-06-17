e54fa226563b0a375f4db3ed27638c1e746c8d1c
Jdilsham <janithadilsham@gmail.com>
2025-06-17 15:40:28 +0530

    Update about.php

---
+++ about.php
@@ -0,0 +1,19 @@
+<?php
+    $conn = mysqli_connect('localhost','root');
+    if ($conn) {
+        echo "Connection Successful";
+    }else{
+        echo "Connection Failed";
+    }
+
+    mysqli_select_db($conn,'website');
+
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $number = $_POST['number'];
+
+    $query = "INSERT INTO users (name,email,number) VALUES ('$name','$email','$number')";
+
+    mysqli_query($conn,$query);
+    header('Location:index.php');
+?>
